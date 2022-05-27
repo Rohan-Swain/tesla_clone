c87a5db493447ffb6dec111a9868f63018a687c5
Rohan Swain <95295970+Rohan-Swain@users.noreply.github.com>
2022-05-27 09:20:24 +0530

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Section from './Section'
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Section />
+        </div>
+    )
+}
+
+export default Home
