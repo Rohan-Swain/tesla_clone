4222ac421b96ea9c39541f6f45806bb273dd9ad0
Rohan Swain <95295970+Rohan-Swain@users.noreply.github.com>
2022-05-27 09:52:08 +0530

    Adding Product Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,79 @@ import React from 'react'
 import Header from './Header'
 import Section from './Section'
 
+const arr = [
+    {
+        title: "Model 3",
+        description: `Order Online for Touchless Delivery`,
+        btn1: "Custom Order",
+        btn2: "Existing Inventory",
+        bgImg: 'model-3.jpg',
+        arr: true
+    },
+    {
+        title: "Model Y",
+        description: "Order Online for Touchless Delivery",
+        btn1: "Custom Order",
+        btn2: "Existing Inventory",
+        bgImg: 'model-y.jpg'
+    },
+    {
+        title: "Model S",
+        description: "Order Online for Touchless Delivery",
+        btn1: "Custom Order",
+        btn2: "Existing Inventory",
+        bgImg: 'model-s.jfif'
+    },
+    {
+        title: "Model X",
+        description: "Order Online for Touchless Delivery",
+        btn1: "Custom Order",
+        btn2: "Existing Inventory",
+        bgImg: 'model-x.jfif'
+    },
+    {
+        title: "Solar Panels",
+        description: "Lowest Cost Solar Panels in America",
+        btn1: "Order now",
+        btn2: "Learn more",
+        bgImg: "solar-panel.jpg"
+    },
+    {
+        title: "Solar Roof",
+        description: "Produce Clean Energy From Your Roof",
+        btn1: "Order now",
+        btn2: "Learn now",
+        bgImg: "solar-roof.jpg"
+    },
+    {
+        title: "Accessories",
+        btn1: "Shop now",
+        bgImg: "accessories.jpg",
+        color: "black"
+    }
+];
+
+function Mapping() {
+    return arr.map(item =>
+        <Section
+            key={arr.indexOf(item).toString()}
+            title={item.title}
+            description={item.description}
+            btn1={item.btn1}
+            btn2={item.btn2}
+            bgImg={item.bgImg}
+            arr={item.arr}
+            color={item.color}
+        />);
+}
+
 function Home() {
     return (
         <div>
             <Header />
-            <Section />
+            <Mapping />
         </div>
-    )
+    );
 }
 
 export default Home
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,86 @@
 import React from 'react'
+import styled from 'styled-components';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-function Section() {
+function Section({ title, description, btn1, btn2, bgImg, arr, color }) {
     return (
-        <div>Section</div>
-    )
+        <SectionWraper bgImg={bgImg}>
+            <Intro>
+                <h1>{title}</h1>
+                <span>{description}</span>
+            </Intro>
+            <Container>
+                <ButtonContainer>
+                    <CustomButton color={"white"} bgColor={!color ? "rgba(23, 26, 32, 0.8)" : color}>{btn1}</CustomButton>
+                    {btn2 && <CustomButton color={"black"} bgColor={"white"}>{btn2}</CustomButton>}
+                </ButtonContainer>
+                {arr && <ArrowDown fontSize="large" />}
+            </Container>
+        </SectionWraper>
+    );
 }
 
 export default Section
+
+const SectionWraper = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background-size: cover;
+    background-position: center;
+    background-image: url("/images/${props => props.bgImg}");
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Intro = styled.div`
+    padding: 15vh;
+    text-align: center;
+
+    h1 {
+        font-size: 40px;
+        margin-bottom: 10px;
+    }
+`;
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 30px;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    margin-bottom: 1.8em;
+    @media (max-width: 600px) {
+        flex-direction: column;
+    }
+`;
+
+const CustomButton = styled.div`
+    background-color: ${(props) => props.bgColor};
+    height: 45px;
+    width: 256px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+    color: ${(props) => props.color};
+    opacity: 0.85;
+    font-weight: bold;
+    user-select: none;
+    cursor: pointer;
+    border-radius: 100px;
+    margin: 8px;
+    font-size: 13px;
+    @media (max-width: 600px) {
+        width: 80vw;
+    }
+`;
+
+const ArrowDown = styled(KeyboardArrowDownIcon)`
+    animation: animateDown infinite 1.5s;
+`;
