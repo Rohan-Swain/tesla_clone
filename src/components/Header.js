53f3ffb52ab0cd804ab7fe07c2049186cb3ae116
Rohan Swain <95295970+Rohan-Swain@users.noreply.github.com>
2022-05-27 09:59:03 +0530

    Adding Side Bar Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
+import LanguageIcon from '@mui/icons-material/Language';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Header() {
+    const [menuStatus, setMenuStatus] = React.useState(false);
 
     return (
         <HeaderWraper>
@@ -19,8 +22,35 @@ function Header() {
             <RightMenu>
                 <span className="hideOnSmallScreen">Shop</span>
                 <span className="hideOnSmallScreen">Account</span>
-                <span>Menu</span>
+                <span onClick={() => setMenuStatus(true)}>Menu</span>
             </RightMenu>
+            <SideBarMenu menuStatus={menuStatus}>
+                <CloseButtonWrap>
+                    <CloseIcon style={{ cursor: "pointer" }} fontSize='large' onClick={() => setMenuStatus(false)} />
+                </CloseButtonWrap>
+                <div>Existing Inventory</div>
+                <div>Used Inventory</div>
+                <div>Trade-In</div>
+                <div>Test Drive</div>
+                <div>Insurance</div>
+                <div>Cybertruck</div>
+                <div>Roadster</div>
+                <div>Semi</div>
+                <div>Charging</div>
+                <div>Powerwall</div>
+                <div>Commercial Energy</div>
+                <div>Utilities</div>
+                <div>Find Us</div>
+                <div>Support</div>
+                <div>Investor Relations</div>
+                <div id='languageWrap'>
+                    <GlobeIcon fontSize="large" />
+                    <div>
+                        <div>United States</div>
+                        <h4>English</h4>
+                    </div>
+                </div>
+            </SideBarMenu>
         </HeaderWraper>
     );
 }
@@ -78,4 +108,49 @@ const MiddleMenu = styled.div`
     @media (max-width: 1200px) {
         display: none;
     }
+`;
+
+const SideBarMenu = styled.div`
+    margin: 0;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+    width: 20em;
+    transition: .4s ease all;
+    transform: ${props => props.menuStatus ? 'translateX(0)' : 'translateX(100%)'};
+    padding-left: 30px;
+
+    div {
+        list-style: none;
+        padding: 10px;
+        font-size: 17px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    #languageWrap {
+        display: flex;
+        align-items: center;
+
+        div {
+            padding: 0;
+            font-weight: bold;
+        }
+
+        h4 {
+            font-weight: normal;
+            margin: 0;
+        }
+    }
+`;
+
+const GlobeIcon = styled(LanguageIcon)`
+    padding: 0px 10px 0px 0px;
+`;
+
+const CloseButtonWrap = styled.div`
+    padding: 30px !important;
+    text-align: end;
 `;
